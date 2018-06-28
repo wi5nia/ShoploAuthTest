0a014a2485647a8d3867a2d16d4f1a970562fe7e
Tomasz Wiśniewski <tomaszwi@microsoft.com>
2018-06-28 13:22:52 +0200

    Initial commit

---
+++ ShoploAuth/ShoploAuth/Program.cs
@@ -0,0 +1,66 @@
+﻿using OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoploAuth
+{
+    class Program
+    {
+        private static string ConsumerKey = string.Empty;
+        private static string ConsumerSecret = string.Empty;
+
+        private static string url = "http://api.shoplo.com/services/oauth/request_token";
+
+        static void Main(string[] args)
+        {
+            while (string.IsNullOrEmpty(ConsumerKey))
+            {
+                Console.WriteLine("Please provide the Consumer Key:");
+                ConsumerKey = Console.ReadLine();
+            }
+
+            while (string.IsNullOrEmpty(ConsumerSecret))
+            {
+                Console.WriteLine("Please provide the Consumer Secret:");
+                ConsumerSecret = Console.ReadLine();
+            }
+
+            CallService();
+
+            Console.ReadLine();
+            
+        }
+
+        private static async void CallService()
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri(url);
+                client.DefaultRequestHeaders.Accept.Clear();
+                var response = client.PostAsync(GetRequestToken(url), new FormUrlEncodedContent(new Dictionary<string, string>())).GetAwaiter().GetResult();
+                string content = await response.Content.ReadAsStringAsync();
+
+                Console.WriteLine(content);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private static string GetRequestToken(string url)
+        {
+            OAuthRequest client = OAuthRequest.ForRequestToken(ConsumerKey, ConsumerSecret);
+            client.RequestUrl = url;
+            client.Method = "POST";
+            client.SignatureMethod = OAuthSignatureMethod.HmacSha1;
+            string urlRet = client.GetAuthorizationQuery();
+            return url + '?' + urlRet;
+        }
+    }
+}
---
+++ ShoploAuth/ShoploAuth/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ShoploAuth")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ShoploAuth")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("74bf5b48-ac25-4a14-b12b-0fa8ac65c703")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
